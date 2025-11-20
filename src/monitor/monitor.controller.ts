242ad807533ea0d6dea39e52d33f6b226c361dd5
PrayushAwale <prayushawale@gmail.com>
2025-11-20 08:48:28 +0545

    added mulitple emails service

--- src/monitor/monitor.controller.ts
+++ src/monitor/monitor.controller.ts
@@ -1,9 +1,46 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { EmailService } from './services/email.service';
 
 @Controller('monitor')
 export class MonitorController {
+  constructor(
+    private sendMail: EmailService,
+    private config: ConfigService,
+  ) {}
+
   @Get()
   getResponse() {
     return 'monitor api route';
+  }
+
+  @Post()
+  sendEmail() {
+    const sendToEmail = this.config.get('SEND_TO_EMAIL').split(':');
+    return this.sendMail.sendMessage(
+      {
+        backendUp: false,
+        createdAt: new Date(),
+        frontendUp: true,
+        lastChecked: new Date(),
+        overallUp: false,
+        site: {
+          name: 'Fuji',
+          location: 'USA',
+          frontend_url: 'https://frontend-fuji.chowchownow.com/',
+          backend_url: 'https://api-fuji.chowchownow.com/',
+          code_name: '0017',
+          verification_phone_number: '',
+          logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTsYKdBXL69yll6ucTRfaUPGt5QmJhxgua_A&s',
+          id: 2,
+          created_date: '2025-08-31T05:02:00.021733',
+          updated_date: '2025-09-02T12:57:54.672166',
+        },
+        siteId: 4,
+        updatedAt: new Date(),
+      },
+      'Alert',
+      sendToEmail,
+    );
   }
 }
